0dc8915813a5f79083685ab76a57b482cbd89398
Seu nome <yago_bohmcruz@hotmail.com>
2024-09-03 07:32:50 -0300

    Setup inicial do Redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import store from "./redux/store.ts";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
